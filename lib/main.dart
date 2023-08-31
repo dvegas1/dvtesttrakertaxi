c27438c1ee9c60aed93ff3784df5fb317f686437
dvegas1 <33332525+dvegas1@users.noreply.github.com>
2023-08-31 00:52:26 -0400

    Ajustes y ejemplos funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,151 @@
-import 'package:flutter/material.dart';
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-import 'package:device_info_plus/device_info_plus.dart';
+import 'dart:io';
+
+import 'package:dvtesttaxi/page.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:mapbox_search/mapbox_search.dart';
+import 'animate_camera.dart';
+import 'annotation_order_maps.dart';
+import 'click_annotations.dart';
+import 'custom_marker.dart';
+import 'full_map.dart';
+import 'layer.dart';
+import 'line.dart';
+import 'map_ui.dart';
+import 'move_camera.dart';
+import 'offline_regions.dart';
+import 'page.dart';
+import 'place_batch.dart';
+import 'place_circle.dart';
+import 'place_fill.dart';
+import 'place_symbol.dart';
+import 'scrolling_map.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final List<ExamplePage> _allPages = <ExamplePage>[MapUiPage(),
+  FullMapPage(),
+  AnimateCameraPage(),
+  MoveCameraPage(),
+  PlaceSymbolPage(),
+  LinePage(),
+  LayerPage(),
+  PlaceCirclePage(),
+  PlaceFillPage(),
+  ScrollingMapPage(),
+  OfflineRegionsPage(),
+  AnnotationOrderPage(),
+  CustomMarkerPage(),
+  BatchAddPage(),
+  ClickAnnotationPage()];
 
-  // This widget is the root of your application.
+class MapsDemo extends StatefulWidget {
+  // FIXME: You need to pass in your access token via the command line argument
+  // da
+  // It is also possible to pass it in while running the app via an IDE by
+  // passing the same args there.
+  //
+  // Alternatively you can replace `String.fromEnvironment("ACCESS_TOKEN")`
+  // in the following line with your access token directly.
+
+  static const String ACCESS_TOKEN ="pk.eyJ1IjoiZGFyd2ludmVnYXMxIiwiYSI6ImNsa2xsNnplcDA2ZGkzanBqeGFjcnlxOW4ifQ.qH6iAzU8UmoSYG7gTJ8f8A";
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MapsDemo> createState() => _MapsDemoState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class _MapsDemoState extends State<MapsDemo> {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void initState() {
+    super.initState();
   }
 
+  /// Determine the android version of the phone and turn off HybridComposition
+  /// on older sdk versions to improve performance for these
+  ///
+  /// !!! Hybrid composition is currently broken do no use !!!
+  Future<void> initHybridComposition() async {
+    if (!kIsWeb && Platform.isAndroid) {
+      final androidInfo = await DeviceInfoPlugin().androidInfo;
+      final sdkVersion = androidInfo.version.sdkInt;
+      if (sdkVersion != null && sdkVersion >= 29) {
+        MapboxMap.useHybridComposition = true;
+      } else {
+        MapboxMap.useHybridComposition = false;
+      }
+    }
+  }
+
+  void _pushPage(BuildContext context, ExamplePage page) async {
+    if (!kIsWeb) {
+      final double lat = 0.0;
+      final double lng = 0.0;
+
+      final Location location =  Location(lat: lat, lng: lng);
+      /*final hasPermissions = await location.hasPermission();
+      if (hasPermissions != PermissionStatus.granted) {
+        await location.requestPermission();
+      }*/
+    }
+    Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (_) => Scaffold(
+          appBar: AppBar(title: Text(page.title)),
+          body: page,
+        )));
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(title: const Text('MapboxMaps examples')),
+      body: MapsDemo.ACCESS_TOKEN.isEmpty ||
+          MapsDemo.ACCESS_TOKEN.contains("YOUR_TOKEN")
+          ? buildAccessTokenWarning()
+          : ListView.separated(
+        itemCount: _allPages.length,
+        separatorBuilder: (BuildContext context, int index) =>
+        const Divider(height: 1),
+        itemBuilder: (_, int index) => ListTile(
+          leading: _allPages[index].leading,
+          title: Text(_allPages[index].title),
+          onTap: () => _pushPage(context, _allPages[index]),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget buildAccessTokenWarning() {
+    return Container(
+      color: Colors.red[900],
+      child: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            "Please pass in your access token with",
+            "--dart-define=ACCESS_TOKEN=ADD_YOUR_TOKEN_HERE",
+            "passed into flutter run or add it to args in vscode's launch.json",
+          ]
+              .map((text) => Padding(
+            padding: EdgeInsets.all(8),
+            child: Text(text,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white)),
+          ))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(home: MapsDemo()));
 }
